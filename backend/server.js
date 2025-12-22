81fe391ad196952333ac630b03257c35a5871835
ahmedkhan <ahmedkhanofficials@gmail.com>
2025-12-23 01:42:34 +0500

    backend

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,7 @@ app.use("/api/admin", adminRoutes);
 
 dbconnect();
 
-const PORT = 5000
-
-
- 
-
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
